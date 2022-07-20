0db17c7053d7e3963b08751c1b070b98bfa4f650
nithinvisva <visvanic@gmail.com>
2022-07-20 07:52:07 +0530

    initial commit

---
+++ src/app/crud-operations/crud-operations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-operations',
+  templateUrl: './crud-operations.component.html',
+  styleUrls: ['./crud-operations.component.scss']
+})
+export class CrudOperationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
